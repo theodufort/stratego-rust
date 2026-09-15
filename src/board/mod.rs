55cca9af84145f2cf21e35e7cfc41054748a410b
Théo Dufort <theodufort05@gmail.com>
2026-09-14 21:08:58 -0400

    init

---
+++ src/board/mod.rs
@@ -0,0 +1,13 @@
+
+struct Board{
+    //Dimensions of the board.
+    dim_x: u8,
+    dim_y: u8,
+    tiles: [[u8; Self::dim_x]; Self::dim_y]
+}
+
+impl Board{
+    pub fn new() -> Board{
+        
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+
+pub mod board;
+fn main() {
+    println!("Hello, world!");
+}
